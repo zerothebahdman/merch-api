9cd735d67579fee155b7eea36ac10bb918e27be9
Amadebusuyi <amadebusuyi@gmail.com>
2022-09-09 08:44:12 +0100

    Open file upload endpoint

--- src/routes/v1/file.route.js
+++ src/routes/v1/file.route.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const fileValidation = require('../../validations/file.validation');
 const fileController = require('../../controllers/file.controller');
 
 const router = express.Router();
 
-router.post('/', auth('creator'), validate(fileValidation.uploadFiles), fileController.uploadBase64File);
+router.post('/', validate(fileValidation.uploadFiles), fileController.uploadBase64File);
 
 module.exports = router;
 
